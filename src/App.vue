acbad014b5c8502ae8cc9fdf6337225cdb840e70
Sasha Kaverina <sasha.kaverina@lewagon.org>
2022-01-29 10:27:38 +0900

    initial setup

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
 </template>
 
 <script>
+import TaskList from './components/TaskList.vue'
 export default {
   name: "App",
 };
---
+++ src/components/TaskList.vue
@@ -0,0 +1,5 @@
+<template>
+    <div id="tasks">
+        <p>This is my task list component</p>
+    </div>
+</template>
