1510c3f96aaa2fd98a0533295b95a454a29875df
Sasha Kaverina <sasha.kaverina@lewagon.org>
2022-01-30 21:05:32 +0900

    visible form

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,6 +1,21 @@
 <template>
     <div id="tasks">
-        <button class="btn round-icon" @click="addTask('My new task', 'Desc')">+</button>
+        <button class="btn round-icon"
+        @click="newFormVisible = !newFormVisible">+</button>
+
+        <div v-if="newFormVisible" class="task-card new-task" @keyup.enter="addTask(newTitle, newDesc), resetForm()">
+            <div>
+                <input
+                type="text"
+                placeholder="What would you like to do?"
+                v-model="newTitle"
+                />
+                <textarea
+                placeholder="Add some details about your task..."
+                v-model="newDesc"
+                ></textarea>
+            </div>
+        </div>
         <TaskCard 
         v-for="(task, index) in tasks"
         :key="index"
@@ -28,6 +43,9 @@ export default {
   data() {
   return {
     tasks: [],
+    newTitle: "",
+    newDesc: "",
+    newFormVisible: false,
   };
 },
 
@@ -35,8 +53,26 @@ methods: {
     addTask(title, description, done = false){
         this.tasks.unshift({title, description,done});
     },
+    resetForm() {
+        this.newTitle = "";
+        this.newDesc = "";
+    },
   },
 };
 
 
 </script>
+
+<style lang="scss">
+@keyframes expand-vertical { from { min-height: 0; height: 0; } to { min-height: 6rem; } }
+.task-card.new-task {
+  animation: expand-vertical 0.2s;
+  overflow: hidden;
+  background-color: white;
+  border-left: solid 5px #35495e;
+  &, &:hover { transform: scale(1.1); box-shadow: 2px 3px 10px rgba(black, 0.2); }
+  & + .tasks-list { pointer-events: none; }
+  input { font-size: 1.17rem; font-weight: bold; width: 100%; }
+  textarea { width: 100%; font-size: 0.9rem; resize: none; }
+}
+</style>
